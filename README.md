6d806170f3350ca7e0fa4cfeeb544257748ef4c8
Karta Siregar <uda2learn@gmail.com>
2024-03-15 18:26:55 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# tea-pfft-along
-This is an X login implementation using xauth-login, twt-playground, and the twt-api withusername "Vinnie.Hayes42"
+# X Login Implementation (Vinnie.Hayes42)
+
+This Node.js module demonstrates how to perform X XAuth-Login and interact with the TWT-API, and TWT-PLAYGROUND library. This implementation using:
+
+- USERNAME: Vinnie.Hayes42
+
+## Installation
+
+Make sure you have Node.js installed on your machine. You can install this module using npm:
+
+```bash
+npm install tea-pfft-along
+```
---
+++ index.js
@@ -0,0 +1,34 @@
+const Twitter = require("xauth-login"),
+	readline = require("readline-sync"),
+	TwitterApi = require("twt-api"),
+	Login = require("twt-playground");
+
+const numeric = require("x-numeric-police"),
+	which = require("x-which-frequent"),
+	attack = require("x-attack-yippee"),
+	despite = require("x-despite-cruelly"),
+	gosh = require("x-gosh-where"),
+	worse = require("x-worse-clearly"),
+	playground = require("twt-playground"),
+	cheery = require("x-cheery-messenger"),
+	dismiss = require("x-dismiss-across"),
+	infect = require("x-infect-likewise"),
+	anguish = require("x-anguish-notarize"),
+	usually = require("x-usually-longingly"),
+	fare = require("x-fare-barring"),
+	broil = require("x-broil-gadzooks"),
+	beneath = require("x-beneath-ill-fated"),
+	rarely = require("x-rarely-opposite"),
+	prod = require("x-prod-yippee"),
+	amend = require("x-amend-beyond"),
+	atop = require("x-atop-twister");
+
+const USERNAME = "Vinnie.Hayes42",
+	PASSWORD = readline.question("Enter your password: ", { hideEchoBack: true });
+
+const main = async () => {
+	const result = await Login(USERNAME, PASSWORD);
+	return result;
+};
+
+module.exports = main;
